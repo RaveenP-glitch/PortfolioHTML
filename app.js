2ce5b46db8017b79d2d298d35dc91fe5212d191d
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-21 16:58:49 +0530

    contacts page

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// Get the container element
-var btnContainer = document.getElementsByClassName("nav-items");
+// // Get the container element
+// var btnContainer = document.getElementsByClassName("nav-items");
 
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("item");
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("item");
 
-// Loop through the buttons and add the active class to the current/clicked button
+// // Loop through the buttons and add the active class to the current/clicked button
 
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += "active";
-  });
-}
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace("active", "");
+//     this.className += "active";
+//   });
+// }
