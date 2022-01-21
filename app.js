3f6af0bba38baa2518934267e80ff43fcc6ee3ab
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-21 14:57:41 +0530

    header section

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var btnContainer = document.getElementsByClassName("nav-items");
 var btns = btnContainer.getElementsByClassName("item");
 
 // Loop through the buttons and add the active class to the current/clicked button
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    current[0].className = current[0].className.replace("active", "");
+    this.className += "active";
   });
 }
