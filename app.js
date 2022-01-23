e4f33c8d96365e9fb5bb9be39bfe8f2ccb4b8537
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-23 19:33:07 +0530

    navbar

--- app.js
+++ app.js
@@ -13,3 +13,12 @@
 //     this.className += "active";
 //   });
 // }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
